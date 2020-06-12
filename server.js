b7c98997b772c00aaa6dd8a1899b1aa8bedb682d
Ashutosh Bhardwaj <theashutosh16@gmail.com>
2020-06-12 19:28:51 +0530

    adding mongo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const connectDB = require('./config/db')
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
